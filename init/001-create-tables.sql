b88ffd1a32e5c31b1e3677105aafec8871807997
hppRC <hpp.ricecake@gmail.com>
2019-12-29 00:17:08 +0900

    add configurations and settings

---
+++ init/001-create-tables.sql
@@ -0,0 +1,9 @@
+USE kadai;
+CREATE TABLE "供給"
+(
+    "部門番号" int(11) NOT NULL,
+    "部品番号" int(11) NOT NULL,
+    "業者番号" int(11) NOT NULL,
+    "単価" int(11) NOT NULL,
+    "数量" int(11) NOT NULL
+);
---
+++ init/init-database.sh
@@ -0,0 +1,2 @@
+mysql -uroot -proot kadai < "/docker-entrypoint-initdb.d/001-create-tables.sql"
+touch test.test
